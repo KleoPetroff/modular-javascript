803187aaecd55adc83c826363e50541418d7a407
KleoPetroff <kleopetroff@gmail.com>
2015-10-27 23:20:41 +0200

    add ES6 modules examples

---
+++ ES6 Modules/example/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import * as mod from 'module';
+
+console.log(mod.helloWorld());
+console.log(mod.divide(15,3));
+console.log(mod.LITERS);
---
+++ ES6 Modules/example/module.js
@@ -0,0 +1,9 @@
+export function helloWorld() {
+    return 'Hello World!';
+}
+
+export const LITERS = 2;
+
+export function devide(num1, num2) {
+    return num1 / num2;
+}
